d002f1bbba2e3847be54326a8109e165b05e8b7c
matthieuEv <evainmatthieu44@gmail.com>
2025-01-19 21:17:09 +0100

    doc: update example

--- example.go
+++ example.go
@@ -1,15 +1,31 @@
 package main
 
-import "github.com/matthieuEv/go-sha256/sha256"
+import (
+	"github.com/matthieuEv/go-sha256/sha256"
+	"fmt"
+	"io"
+	"os"
+)
 
 func example(){
 	// sha256.DEBUG = true
 	var msg = []byte("The quick brown fox jumps over the lazy dog")
 	sha := sha256.Sum256(msg)
-	print(sha)
-	print("\n")
+	fmt.Println(sha)
 	var msg2 = []byte("hello world")
 	sha2 := sha256.Sum256(msg2)
-	print(sha2)
-	print("\n")
+	fmt.Println(sha2)
+
+	// Test with a file
+	f, err := os.Open("sha256/const.go")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	data, err := io.ReadAll(f)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(sha256.Sum256(data))
 }
